39c2a3fcd52944df676db7a8811f5abb1b153cf8
KnotSarvajit <sarvajitvisagan@gmail.com>
2023-06-16 18:26:51 +0530

    made changes to dowload zip of participants and winners

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, make_response
 from . import db
 from flask_login import login_required, current_user
 from .models import User, Event
@@ -7,6 +7,7 @@ import os
 import cv2
 import zipfile
 import numpy as np
+from io import BytesIO
 
 app = Blueprint('app', __name__)
 
@@ -28,7 +29,7 @@ def dash():
 def create_event():
     event_name = request.form.get('event_name')
     brandings = request.form.get('brandings')
-    
+
     # Save certificate templates
     certificate_design_participation = request.files['certificate_design_participation']
     certificate_design_winner = request.files['certificate_design_winner']
@@ -36,18 +37,18 @@ def create_event():
     # Generate secure filenames and save templates
     filename_participation = secure_filename(certificate_design_participation.filename)
     filename_winner = secure_filename(certificate_design_winner.filename)
-    certificate_design_participation.save(os.path.join('CertificateGenerator', 'Templates', filename_participation))
-    certificate_design_winner.save(os.path.join('CertificateGenerator', 'Templates', filename_winner))
+    certificate_design_participation.save(os.path.join('CertificateGenerator', filename_participation))
+    certificate_design_winner.save(os.path.join('CertificateGenerator', filename_winner))
 
     # Example participant and winner names
     participants = request.form.getlist('participants')
     winners = request.form.getlist('winners')
 
     # Load the participation certificate template
-    certificate_participation = cv2.imread(os.path.join('CertificateGenerator', 'Templates', filename_participation))
+    certificate_participation = cv2.imread(os.path.join('CertificateGenerator', filename_participation))
 
     # Load the winner certificate template
-    certificate_winner = cv2.imread(os.path.join('CertificateGenerator', 'Templates', filename_winner))
+    certificate_winner = cv2.imread(os.path.join('CertificateGenerator', filename_winner))
 
     # Function to place the name on the certificate at an appropriate location
     def place_name_on_certificate(certificate, name):
@@ -63,28 +64,28 @@ def create_event():
 
         return certificate
 
-    # Add participant names to the participation certificate
-    for i, participant in enumerate(participants):
-        certificate_participation_copy = certificate_participation.copy()
-        certificate_participation_copy = place_name_on_certificate(certificate_participation_copy, participant)
-        cv2.imwrite(os.path.join('CertificateGenerator', 'Templates', f'participant_certificate_{i}.png'), certificate_participation_copy)
-
-    # Add winner names to the winner certificate
-    for i, winner in enumerate(winners):
-        certificate_winner_copy = certificate_winner.copy()
-        certificate_winner_copy = place_name_on_certificate(certificate_winner_copy, winner)
-        cv2.imwrite(os.path.join('CertificateGenerator', 'Templates', f'winner_certificate_{i}.png'), certificate_winner_copy)
-
-    # Create a zip file
-    with zipfile.ZipFile(os.path.join('CertificateGenerator', 'certificates.zip'), 'w') as zip_file:
+    # Create an in-memory zip file
+    zip_buffer = BytesIO()
+    with zipfile.ZipFile(zip_buffer, 'w') as zip_file:
         # Add participant certificates to the zip file
-        for i in range(len(participants)):
-            zip_file.write(os.path.join('CertificateGenerator', 'Templates', f'participant_certificate_{i}.png'),
-                           f'participant_certificate_{i}.png')
+        for i, participant in enumerate(participants):
+            certificate_participation_copy = certificate_participation.copy()
+            certificate_participation_copy = place_name_on_certificate(certificate_participation_copy, participant)
+            certificate_data = cv2.imencode('.png', certificate_participation_copy)[1]
+            zip_file.writestr(f'participant_certificate_{i}.png', certificate_data)
 
         # Add winner certificates to the zip file
-        for i in range(len(winners)):
-            zip_file.write(os.path.join('CertificateGenerator', 'Templates', f'winner_certificate_{i}.png'),
-                           f'winner_certificate_{i}.png')
+        for i, winner in enumerate(winners):
+            certificate_winner_copy = certificate_winner.copy()
+            certificate_winner_copy = place_name_on_certificate(certificate_winner_copy, winner)
+            certificate_data = cv2.imencode('.png', certificate_winner_copy)[1]
+            zip_file.writestr(f'winner_certificate_{i}.png', certificate_data)
 
-    return redirect(url_for('app.dash'))
+    zip_buffer.seek(0)  # Reset the buffer position to the beginning
+
+    # Create a response with the zip file data
+    response = make_response(zip_buffer.getvalue())
+    response.headers['Content-Disposition'] = 'attachment; filename=certificates.zip'
+    response.headers['Content-type'] = 'application/zip'
+
+    return response
